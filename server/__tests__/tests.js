91a70a70f102a2c7443d9f65488f595269259965
Alex Huang <alex@clinical.ly>
2023-12-15 19:45:04 -0500

    backend testing

--- server/__tests__/tests.js
+++ server/__tests__/tests.js
@@ -4,83 +4,69 @@ const app = require('../server');
 const results = require("./results.json")
 
 test('GET /stock/AAPL', async () => {
-  await supertest(app).get('/stock/AAPL')
+  await supertest(app).get('/stock/?stocks=AAPL')
     .expect(200)
     .then((res) => {
-      expect(res.body).toStrictEqual(results.stock)
+      // Use arrayContaining to check that a subset of an array is in the response
+      expect(res.body).toEqual(expect.arrayContaining(results.stock));
     });
 });
 
-test('GET /stock/AAPL', async () => {
-  await supertest(app).get('/stock/AAPL')
-    .expect(200)
-    .then((res) => {
-      expect(res.body).toStrictEqual(results.stock)
-    });
-});
 
-test('GET /market_cap', async () => {
-  await supertest(app).get('/market_cap')
+test('GET /market_cap/AAPL', async () => {
+  await supertest(app).get('/market_cap/?stocks=AAPL')
     .expect(200)
     .then((res) => {
       expect(res.body).toStrictEqual(results.market_cap)
     });
 });
 
-test('GET /pos_pct_change', async () => {
-  await supertest(app).get('/pos_pct_change')
+test('GET /pos_pct_change/GOOG', async () => {
+  await supertest(app).get('/pos_pct_change/?stocks=GOOGL')
     .expect(200)
     .then((res) => {
       expect(res.body).toStrictEqual(results.pos_pct_change)
     });
 });
 
-test('GET /neg_pct_change', async () => {
-  await supertest(app).get('/neg_pct_change')
+test('GET /neg_pct_change/GOOG', async () => {
+  await supertest(app).get('/neg_pct_change/?stocks=GOOGL')
     .expect(200)
     .then((res) => {
       expect(res.body).toStrictEqual(results.neg_pct_change)
     });
 });
 
-test('GET /index_closing', async () => {
-  await supertest(app).get('/index_closing/NYA')
+test('GET /index_closing/NYA', async () => {
+  await supertest(app).get('/index_closing/?indexes=NYA')
     .expect(200)
     .then((res) => {
-      const expectedResult = {"date":"1986-12-31T05:00:00.000Z","marketIndex":"NYA","closeUSD":1465.310059};
-
-      // Assuming the response body is an array of objects
-      const resultArray = res.body;
-
-      // Check if the resultArray includes the expected result
-      const includesExpectedResult = resultArray.some(item => (
-        item.date === expectedResult.date &&
-        item.marketIndex === expectedResult.marketIndex &&
-        item.closeUSD === expectedResult.closeUSD
-      ));
-
-      expect(includesExpectedResult).toBe(true);
+      // Use arrayContaining to check that a subset of an array is in the response
+      expect(res.body).toEqual(expect.arrayContaining(results.index_closing));
     });
 });
 
+
 test('GET /single_day_pct', async () => {
-  await supertest(app).get('/single_day_pct')
+  await supertest(app).get('/single_day_pct/?stocks=GOOGL')
     .expect(200)
     .then((res) => {
       expect(res.body).toStrictEqual(results.single_day_pct)
     });
 });
 
+jest.setTimeout(100000)
 test('GET /vol', async () => {
-  await supertest(app).get('/vol')
+  await supertest(app).get('/vol/')
     .expect(200)
     .then((res) => {
       expect(res.body).toStrictEqual(results.vol)
     });
 });
 
+jest.setTimeout(100000)
 test('GET /beta', async () => {
-  await supertest(app).get('/beta/AAPL/NYA')
+  await supertest(app).get('/beta/?stocks=AAPL&index=NYA')
     .expect(200)
     .then((res) => {
       expect(res.body).toStrictEqual(results.beta)
@@ -89,7 +75,7 @@ test('GET /beta', async () => {
 
 
 test('GET /stock_index_corr', async () => {
-  await supertest(app).get('/stock_index_corr/AAPL/NYA')
+  await supertest(app).get('/stock_index_corr/?stocks=AAPL&index=NYA')
     .expect(200)
     .then((res) => {
       expect(res.body).toStrictEqual(results.stock_index_corr)
@@ -99,7 +85,7 @@ test('GET /stock_index_corr', async () => {
 
 
 test('GET /bol_bands', async () => {
-  await supertest(app).get('/bol_bands/AAPL')
+  await supertest(app).get('/bol_bands/?stocks=AAPL')
     .expect(200)
     .then((res) => {
       const expectedResult = {"name":"AAPL","date":"2014-02-07T05:00:00.000Z","moving_avg":null,"upper_band":null,"lower_band":null};
@@ -118,7 +104,7 @@ test('GET /bol_bands', async () => {
 });
 
 test('GET /macd', async () => {
-  await supertest(app).get('/macd/AAPL')
+  await supertest(app).get('/macd/?stocks=AAPL')
     .expect(200)
     .then((res) => {
       const expectedResult = {"name":"AAPL","date":"2014-02-07T05:00:00.000Z","twelve_days_ema":null,"twenty_six_days_ema":null,"macd_line":null,"macd_histogram":null};
@@ -137,7 +123,7 @@ test('GET /macd', async () => {
 });
 
 test('GET /rel_strength', async () => {
-  await supertest(app).get('/rel_strength/AAPL')
+  await supertest(app).get('/rel_strength/?stocks=GOOGL')
     .expect(200)
     .then((res) => {
       expect(res.body).toStrictEqual(results.rel_strength)
@@ -145,47 +131,25 @@ test('GET /rel_strength', async () => {
 });
 
 test('GET /stock_index_mean_comp', async () => {
-  await supertest(app).get('/stock_index_mean_comp/AAPL/NYA')
-    .expect(200)
-    .then((res) => {
-      const expectedResult = {"avg_daily_stock_return":0.00078619669118,"avg_daily_index_return":0.00030822206399};
-
-      // Assuming the response body is an array of objects
-      const resultArray = res.body;
-
-      // Check if the resultArray includes the expected result
-      const includesExpectedResult = resultArray.some(item => (
-        item.date === expectedResult.date &&
-        item.marketIndex === expectedResult.marketIndex &&
-        item.closeUSD === expectedResult.closeUSD
-      ));
-
-      expect(includesExpectedResult).toBe(true);
-    });
+  await supertest(app).get('/stock_index_mean_comp/?stocks=AAPL&indexes=NYA')
+  .expect(200)
+  .then((res) => {
+    // Use arrayContaining to check that a subset of an array is in the response
+    expect(res.body).toEqual(expect.arrayContaining(results.stock_index_mean_comp));
+  });
 });
 
 test('GET /stock_index_comparison', async () => {
-  await supertest(app).get('/stock_index_comparison/AAPL/NYA')
-    .expect(200)
-    .then((res) => {
-      const expectedResult = {"date":"2013-02-11T05:00:00.000Z","stock_ticker":"AAPL","stock_return":0.0104223467,"index_ticker":"NYA","index_return":-0.0018142719};
-
-      // Assuming the response body is an array of objects
-      const resultArray = res.body;
-
-      // Check if the resultArray includes the expected result
-      const includesExpectedResult = resultArray.some(item => (
-        item.date === expectedResult.date &&
-        item.marketIndex === expectedResult.marketIndex &&
-        item.closeUSD === expectedResult.closeUSD
-      ));
-
-      expect(includesExpectedResult).toBe(true);
-    });
+  await supertest(app).get('/stock_index_comparison/?stocks=AAPL&indexes=NYA')
+  .expect(200)
+  .then((res) => {
+    // Use arrayContaining to check that a subset of an array is in the response
+    expect(res.body).toEqual(expect.arrayContaining(results.stock_index_comparison));
+  });
 });
 
 test('GET /exp_returns', async () => {
-  await supertest(app).get('/exp_returns/AAPL,GOOGL')
+  await supertest(app).get('/exp_returns/?stocks=AAPL')
     .expect(200)
     .then((res) => {
       expect(res.body).toStrictEqual(results.exp_returns)
@@ -195,28 +159,28 @@ test('GET /exp_returns', async () => {
 
 test('GET /stock null', async () => {
   await supertest(app)
-    .get('/stock/acb')
+    .get('/stock/?stocks=acb')
     .expect(200)
     .then((res) => {
       // Assert that the response body includes an error message
-      expect(res.body).toStrictEqual({})
+      expect(res.body).toStrictEqual([])
     });
 });
 
 test('GET /exp_returns null', async () => {
   await supertest(app)
-    .get('/exp_returns/acb')
+    .get('/exp_returns/?stocks=acb')
     .expect(200)
     .then((res) => {
       // Assert that the response body includes an error message
-      expect(res.body).toStrictEqual({})
+      expect(res.body).toStrictEqual([])
     });
 });
 
 test('GET /beta null', async () => {
-  await supertest(app).get('/beta/acb/acb')
+  await supertest(app).get('/beta/?stocks=acb&index=acb')
     .expect(200)
     .then((res) => {
-      expect(res.body).toStrictEqual({})
+      expect(res.body).toStrictEqual([])
     });
 });
